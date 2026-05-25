7341afd08725fe27056956e56fa363713a5e0c57
Dove Stack <flawal664@gmail.com>
2026-05-25 18:05:55 +0100

    deleted unused font family from tailwind config.
    It is already decided to use Inter font family for the project, so we can remove the default font family from tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,21 +6,6 @@ export default {
 
   theme: {
     extend: {
-      fontFamily: {
-        sans: [
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"SF Pro Text"',
-          '"SF Pro Display"', // quotes needed for spaces
-          '"Segoe UI"',
-          "Roboto",
-          "Oxygen",
-          "Ubuntu",
-          "Cantarell",
-          '"Helvetica Neue"',
-          "sans-serif",
-        ],
-      },
       boxShadow: {
         "card-light": "0px 4px 4px 0px rgb(145, 158, 171)",
       },
