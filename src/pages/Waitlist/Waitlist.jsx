326d347d6dc18a8f238793c7be17733e0e637402
Dove Stack <flawal664@gmail.com>
2026-05-25 18:08:52 +0100

    deleting dead code from waitlist page, will be re-implemented in next iteration

--- src/pages/Waitlist/Waitlist.jsx
+++ src/pages/Waitlist/Waitlist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+letimport React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import { Logo } from "../../assets/assets.js";
 import { Link } from "react-router-dom";
