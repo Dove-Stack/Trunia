439a8c0c4aeca547342d1ec002892031d8458202
Dove Stack <flawal664@gmail.com>
2026-05-25 21:16:24 +0100

    Formatted Counter.jsx component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,18 +3,17 @@ import { counterData } from "../../assets/assets";
 
 const Counter = () => {
   return (
-    <div className="
+    <div
+      className="
       flex flex-col
       sm:flex-row sm:flex-wrap
       items-center justify-center
       gap-8 sm:gap-16 lg:gap-40
       mt-10 mb-10
-    ">
+    "
+    >
       {counterData.map((counter, index) => (
-        <div
-          key={index}
-          className="flex flex-col gap-1 text-center min-w-30"
-        >
+        <div key={index} className="flex flex-col gap-1 text-center min-w-30">
           <h3 className="text-2xl sm:text-3xl font-medium text-brand-navy mb-0.5">
             {counter.feat}
           </h3>
