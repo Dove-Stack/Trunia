44e40a1f5efa10dda1e4d54059e71b8e08be0a7e
Dove Stack <flawal664@gmail.com>
2026-05-25 18:20:01 +0100

    deleted old code from App.jsx and replaced it with new code that implements BrowserRouter, Routes, and Route components from react-router-dom to set up routing for the application. The new code also imports HomePage  and [ soon-to-be-deleted ] waitlist page and defines routes for them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,4 @@
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from 'react';
-import axios from 'axios';
 
-function App() {
-  // const [count, setCount] = useState(0)
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/");
-  })
-
-  return (
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-black">
-        <h1 className="text-4xl font-bold text-cyan-400">
-          Tailwind v4 is finally working
-        </h1>
-      </div>
-    </>
-  );
-}
-
-export default App
- */
 
 import React, { useState, useRef } from "react";
 import "./index.css";
